c582d89584fb770f46d5eaa3f70f2104e94e5bad
chasmani <pilgrimcharlie2@gmail.com>
2015-02-04 22:40:26 +0000

    Updated caesar cipher

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,9 +1,9 @@
 #Caesar Cipher
 #Simple text cipher that shifts letters a fixed number of positions
 
-def caesar_cipher(string, shift)
+def caesar_cipher_encrypt(string, shift)
 	
-	#Letter Arrays
+	#Alphabet Arrays
 	alpha = ("a".."z").to_a
 	alphaBig = ("A".."Z").to_a
 	new_string = ""
@@ -25,6 +25,10 @@ def caesar_cipher(string, shift)
 	new_string
 end
 
+def caeser_cipher_decrypt(string, shift)
+	caesar_cipher_encrypt(string, 0-shift)
+end
+
 def letterShift(letter, array, shift)
 #captures letter shift logic
 
@@ -33,8 +37,19 @@ def letterShift(letter, array, shift)
 	new_letter = array[new_index]
 end
 
-##Tests ## 
-puts caesar_cipher("What a string!", 5)
-puts caesar_cipher("Hello", 0)
-puts caesar_cipher("ABCDE_vwxyz@?", 5)
-puts caesar_cipher("ABCDEvwxyz", -3)
+##Tests##
+message = "HI"
+puts "-----Test 1-----" 
+key1 = 2
+encrypted_message1 = caesar_cipher_encrypt(message, key1)
+puts "Encrypted Message 1: #{encrypted_message1}"
+decrypted_message1 = caeser_cipher_decrypt(encrypted_message1, key1)
+puts "Decrypted message 1: #{decrypted_message1}"
+
+puts "-----Test 2-----" 
+message = "HI"
+key2 = 20
+encrypted_message2 = caesar_cipher_encrypt(message, key2)
+puts "Encrypted Message 2: #{encrypted_message2}"
+decrypted_message2 = caeser_cipher_decrypt(encrypted_message2, key2)
+puts "Decrypted message 1: #{decrypted_message2}"
